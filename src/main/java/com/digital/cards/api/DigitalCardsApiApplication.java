196efd9755c93080fe132588a3c8da32bd5f73e5
Victor Jose Arana Rodriguez <arana@localhost.localdomain>
2020-07-31 20:34:49 -0500

    :tada: configure initial project

---
+++ src/main/java/com/digital/cards/api/DigitalCardsApiApplication.java
@@ -0,0 +1,13 @@
+package com.digital.cards.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCardsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCardsApiApplication.class, args);
+	}
+
+}
