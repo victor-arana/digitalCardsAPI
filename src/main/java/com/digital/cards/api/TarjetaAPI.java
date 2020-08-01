5e3e6a672a307aa1a83cc34cf9b2601bf47f9bec
Victor Jose Arana Rodriguez <arana@localhost.localdomain>
2020-07-31 21:20:41 -0500

    :card_file_box: add hardcoded tarjetas list

--- src/main/java/com/digital/cards/api/TarjetaAPI.java
+++ src/main/java/com/digital/cards/api/TarjetaAPI.java
@@ -1,5 +1,8 @@
 package com.digital.cards.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +10,25 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.digital.cards.api.model.Tarjeta;
+
 @RestController
 @RequestMapping("/api/v1/tarjeta")
 public class TarjetaAPI {
+	
+	private static List<Tarjeta> tarjetas = new ArrayList<>();
+	
+	static {
+		Tarjeta tarjeta = new Tarjeta();
+		tarjeta.setId(1);
+		tarjeta.setNombreTitular("Eduardo Castillo");
+		tarjeta.setNombreEntidad("Space Money");
+		tarjeta.setNumeroTarjeta("1234-1234-2143-2344");
+		tarjeta.setMarca("Space VISA");
+		tarjeta.setFechaCaducidad("10/12");
+		
+		tarjetas.add(tarjeta);
+	}
 
 	@PostMapping
 	public void crearTarjeta() {
